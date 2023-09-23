a721910d1b4857c053664b68df9661161cd0756d
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-23 10:35:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const line = document.getElementById('line')
+let angle = 0
+function rotate(){
+  angle+=2
+  line.style.transform = `rotate(${angle}deg)`
+}
+
+setInterval(() => {
+  rotate()
+  console.log("hello")
+}, 20);
